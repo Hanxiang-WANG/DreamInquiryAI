e1bbc39509b239ae6b89afbd788734cb4960645a
Hanxiang-WANG <hanxiangw@student.unimelb.edu.au>
2024-10-02 17:41:48 +1000

    new update

--- ai-saas/components/empty.tsx
+++ ai-saas/components/empty.tsx
@@ -1,11 +1,21 @@
+import Image from "next/image";
+
 interface EmptyProps {
     label: string;
 }
 
-export const Empty = () => {
+export const Empty = ({
+    label
+}: EmptyProps) => {
     return (
-        <div>
-            Empty
+        <div className="h-full p-20 flex flex-col items-center justify-center">
+            <div className="relative h-72 w-72">
+                <Image 
+                    alt='Empty'
+                    fill
+                    src="/empty.png"
+                />
+            </div>
         </div>
     );
 }
