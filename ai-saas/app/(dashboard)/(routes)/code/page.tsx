97881bbc8c5e55faf10daaf6e8d22c31dc1099dc
Hanxiang-WANG <hanxiangw@student.unimelb.edu.au>
2024-10-15 16:34:41 +1100

    code generation complete

--- ai-saas/app/(dashboard)/(routes)/code/page.tsx
+++ ai-saas/app/(dashboard)/(routes)/code/page.tsx
@@ -116,10 +116,21 @@ const CodePage = () => {
                             message.role === "user" ? "bg-white border border-black/10" : "bg-muted")}
                         >
                             {message.role === "user" ? <UserAvatar/> : <BotAvatar/>}
-                            <p className="text-sm">
-                              {message.content}
-                            </p>
-                            
+                            <ReactMarkdown
+                                components={{
+                                    pre: ({ node, ...props }) => (
+                                        <div className="overflow-auto w-full my-2 bg-black/10 p-2 rounded-lg">
+                                            <pre {...props}/>
+                                        </div>
+                                    ),
+                                    code: ({ node, ...props }) => (
+                                        <code className="bg-black/10 rounded-lg p-1" {...props}/>
+                                    )
+                                }}
+                                className="text-sm overflow-hidden leading-7"
+                            >
+                                {message.content || ""}
+                            </ReactMarkdown>
                         </div>
                     ))}
                 </div>
